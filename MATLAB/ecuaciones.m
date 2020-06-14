3cfc05d4707cf3e1af3d994165f14a401a068771
alexitoo00 <alexandrocb2013@gmail.com>
2020-06-14 20:06:47 +0200

    Update and 'user-friendly' do programa para resolver sistemas de ecuacions

--- MATLAB/ecuaciones.m
+++ MATLAB/ecuaciones.m
@@ -1,4 +1,7 @@
 function [f] = ecuaciones(x)
+%   Añadir todas as ecuacions que se requieran aquí debaixo
+%   e anotar cal é o índice de cada variable para escribir
+%   por orden na folla titulada 'resolverSistemaEcuacions.m'
 f(1) = x(1)-x(2);
 f(2) = 2*x(1)-x(2)-2;
 f(3)=x(3)-x(4);
