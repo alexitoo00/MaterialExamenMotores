8379b4adac149c950700dd664f0e69022f61accd
alexitoo00 <47087779+alexitoo00@users.noreply.github.com>
2020-06-14 21:19:59 +0200

    Añadido un ejemplo de Hidraulica

--- MATLAB/ecuaciones.m
+++ MATLAB/ecuaciones.m
@@ -2,6 +2,7 @@ function [f] = ecuaciones(x)
 %   Añadir todas as ecuacions que se requieran aquí debaixo
 %   e anotar cal é o índice de cada variable para escribir
 %   por orden na folla titulada 'resolverSistemaEcuacions.m'
+%   substituindo donde sea necesario os valores dos datos conocidos
 
 %   NOTA:   Escribe a función no formato f(x)=0, si non, non funcionará
 %   
@@ -13,8 +14,13 @@ function [f] = ecuaciones(x)
 %           ecuacións, así que síntete libre de escribir as fórmulas todas
 %           xuntas ou separadas ben e bonitas
 
-f(1) = x(1) - x(2);
-f(2) = 2*x(1) - x(2) - 2;
-f(3)= x(3) - x(4);
+%   RECOMENDACIÓN:  Anota á dereita da ecuación precedida por un símbolo
+%                   de '%' a ecuación que representa para unha
+%                   visualización e comprensión máis clara do sistema
+
+f(1) = x(1) + x(2)-20;              %   q1 + q2 = Q
+f(2) = x(3) - x(4);                 %   H1 = H2
+f(3)= x(3) -2*(63-0.375*x(1)^2);    %   H1 = 2 * (63 - 0.375 * q1^2)
+f(4)= x(4) - 63 +0.375*(x(2)^2);    %   H2 = 63 - 0.375 * q2^2
 
 end
